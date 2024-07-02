bda5ef1b3d2c47b8bd5b18452869111c0619c657
Christopher Helms <cjohnhelms@gmail.com>
2024-07-02 18:52:35 -0500

    feat: added support for doubling down

--- src/flapjack.py
+++ src/flapjack.py
@@ -6,7 +6,9 @@ import time
 import sys
 from os import system, name
 
-TITLE = """
+BACKSIDE: str = 'backside'
+divider: str = '\n' + ('-' * 20) + '\n'
+TITLE: str = """
                                                                                                     
                           ████▓▓████████▓▓████▒▒████████▓▓▓▓▓▓██▓▓██                                  
                     ██████░░░░░░░░▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▓▓▓▓▓▓▒▒██████                            
@@ -66,23 +68,32 @@ def clear():
     else:
         _ = system('clear')
 
-def drawScreen(player: Player, dealer: Dealer, deck: Deck, reveal: bool):
+def exit_msg():
     clear()
-    if reveal:
-        dealer.showHand()
-        print(f'Total: {dealer.value}')
+    print('Thanks for playing!')
+    time.sleep(1)
+    clear()
+    sys.exit()
+
+def draw_screen(player: Player, dealer: Dealer, deck: Deck, player_hidden: bool, dealer_hidden: bool):
+    clear()
+    if dealer_hidden:
+        dealer.display_cards([ BACKSIDE ] + dealer.hand[1:])
     else:
-        dealer.displayHand()
+        player.get_value()
+        dealer.display_cards(dealer.hand)
     print()
     print('-' * 20)
     print()
-    if reveal:
-        print(f'Total: {player.value}')
     print(f'Flapjacks: {player.flapjacks}')
     print(f'Current bet: {player.current_bet}')
-    player.showHand()
+    if player_hidden:
+        player.display_cards([ BACKSIDE ] + player.hand[1:])
+    else:
+        player.get_value()
+        player.display_cards(player.hand)
 
-def initHand(player: Player, dealer: Dealer, deck: Deck):
+def init_hand(player: Player, dealer: Dealer, deck: Deck):
     player.hand = []
     dealer.hand = []
     player.value = 0
@@ -96,54 +107,49 @@ def initHand(player: Player, dealer: Dealer, deck: Deck):
 def main():
     clear()
     print(TITLE)
-    print("-" * 30)
+    print(divider)
     print("Welcome to Flapjack! It's like blackjack, but you're betting with pancakes.\nYou are starting out with 500 flapjacks.")
-    print("-" * 30)
-    print()
+    print(divider)
     deck = Deck()
     deck.shuffle()
     dealer = Dealer()
     player = Player()
     while True:
-        initHand(player, dealer, deck)
+        init_hand(player, dealer, deck)
         if player.flapjacks > 1:
-            player.takeBet()
+            player.take_bet()
         else:
             print('Sorry, you\'re all out of flapjacks.')
             sys.exit()
         while True:
-            drawScreen(player, dealer, deck, False)
-            
-            player.getValue()
+            draw_screen(player, dealer, deck, player.hidden, True)
+ 
+            player.get_value()
             if player.value > 21:
-                print('Bust')
                 break
 
-            move = player.takeMove().upper()
-            if move == 'D':
-                player.current_bet += player.current_bet
+            move: str = player.take_move(deck)
+            match move:
+                case 'h':
+                    continue
+                case 's':
+                    break
+                case 'd':
+                    draw_screen(player, dealer, deck, player.hidden, True)
+                    break
 
-            if move in ('H', 'D'):
-                player.deal(deck)
-
-            if move in ('S', 'D'):
-                break
-
-        player.getValue()
+        player.get_value()
+        dealer.get_value()
         if player.value <= 21:
-            dealer.getValue()
+            dealer.get_value()
             while dealer.value < 17:
-                print('Dealer hits...')
-                time.sleep(2)
-                dealer.deal(deck)
-                drawScreen(player, dealer, deck, False)
+                dealer.hit(deck)
+                draw_screen(player, dealer, deck, player.hidden, True)
 
-                dealer.getValue()
-                if dealer.value > 21:
-                    input('Press Enter to conitue...')
-                    print()
-        
-        drawScreen(player, dealer, deck, True)
+        time.sleep(1)
+        draw_screen(player, dealer, deck, False, False)
+        print(divider)
+        print(f'Player total: {player.value}\tDealer total: {dealer.value}\n')
         if dealer.value > 21:
             print(f'Dealer busts, you won ${player.current_bet}!')
             player.flapjacks += player.current_bet
@@ -157,13 +163,19 @@ def main():
             print('It\'s a tie, the bet is returned to you.')
 
         cont = input('Play again? (y/n) ').lower()
-        if cont == 'y':
-            clear()
-            continue
-        else:
-            print('Thanks for playing!')
-            break
-        
+        while True:
+            if cont not in ['y', 'n']:
+                print("Please select yes or no.")
+            else:
+                if cont == 'y':
+                    clear()
+                    break
+                else:
+                    exit_msg()
+
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit_msg()
 
---
+++ src/player.py
@@ -0,0 +1,109 @@
+from deck import Deck
+import time
+
+BACKSIDE = 'backside'
+
+class Person():
+    def __init__(self):
+        self.hand: list(tuple) = []
+        self.value: int = 0
+        self.flapjacks: int = 500
+        self.current_bet: int = 0
+        self.hidden: bool = False
+
+    def deal(self, deck):
+        self.hand.append(deck.draw())
+
+    def get_value(self):
+        aces: int = 0
+        self.value = 0
+        for card in self.hand:
+            rank = card[0]
+            if rank in ('J', 'Q', 'K'):
+                self.value += 10
+            elif rank == 'A':
+                aces += 1
+            else:
+                self.value += int(rank)
+        if self.value > 10:
+            self.value += aces
+        else:
+            self.value += aces * 10
+
+    def display_cards(self, cards: list[tuple]):
+        rows: list[str] = ['', '', '', '', '', '', '', '', '']
+
+        for i, card in enumerate(cards):
+            rows[0] += ' _______  '
+            if card == BACKSIDE:
+                rows[1] += '|##     | '
+                rows[2] += '|       | '
+                rows[3] += '|       | '
+                rows[4] += '|  ###  | '
+                rows[5] += '|       | '
+                rows[6] += '|       | '
+                rows[7] += '|_____##| '
+            else:
+                rank, suit = card
+                rows[1] += '|{}     | '.format(rank.ljust(2))
+                rows[2] += '|       | '
+                rows[3] += '|       | '
+                rows[4] += '|   {}   | '.format(suit)
+                rows[5] += '|       | '
+                rows[6] += '|       | '
+                rows[7] += '|_____{}| '.format(rank.rjust(2, '_'))
+        for row in rows:
+            print(row)
+
+class Player(Person):
+    def take_bet(self):
+        while True:
+            try:
+                print(f'Flapjacks: {self.flapjacks}')
+                bet: int = int(input("Bet: "))
+                if bet <= self.flapjacks:
+                    self.current_bet = bet
+                    break
+                else:
+                    print('Not enough flapjacks!')
+            except ValueError:
+                print('Please enter a number')
+
+    def take_move(self, deck: Deck):
+        while True:
+            if len(self.hand) == 2 and self.hand[0][0] == self.hand[1][0]:
+                choice: str = input("(H)it, (S)tay, (D)ouble down, or S(p)lit? ")
+            elif len(self.hand) == 2:
+                choice: str =  input("(H)it, (S)tay, or (D)ouble down? ")
+            else:
+                choice: str = input("(H)it or (S)tay? ")
+            if choice.lower() not in ['h', 's', 'd', 'p']:
+                print('Please select a valid option.')
+            else:
+                match choice:
+                    case 'h':
+                        self.deal(deck)
+                        self.hidden = False
+                        return choice
+                    case 's':
+                        self.hidden = False
+                        return choice
+                    case 'd':
+                        self.get_value()
+                        if (self.value - self.current_bet) >= self.current_bet:
+                            self.deal(deck)
+                            self.hidden = True
+                            return choice
+                        else:
+                            print('Not enough flapjacks.')
+
+class Dealer(Person):
+    def __init__(self):
+        Person.__init__(self)
+        self.hidden = True
+
+    def hit(self, deck: Deck):
+        print('Dealer hits...')
+        time.sleep(1)
+        self.deal(deck)
+        self.get_value()
